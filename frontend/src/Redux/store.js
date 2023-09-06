2e80b76953945d4ecaa0c928aac880b6bd9377d6
shreshthkr <shreshthkumar6@gmail.com>
2023-09-06 22:20:25 +0530

    added tv Show data

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,10 +1,12 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import { reducer as movieReducer } from "./Movie/reducer";
+import {reducer as trendingTvreducer} from "./Tv Show/reducer";
 import thunk from "redux-thunk";
 
 
 const rootReducer = combineReducers({
-    movieReducer
+    movieReducer,
+    trendingTvreducer
 });
 
 
